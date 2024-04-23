5285cf70ca5f4d23ff81a61cef1bdb5a40f474e1
Silas Buarque <silasbuarque@gmail.com>
2024-04-23 20:52:15 -0300

    Resolvendo o exercicio de mapas

---
+++ mapas-desafio/src/Estado.java
@@ -0,0 +1,35 @@
+import java.util.Objects;
+
+public class Estado implements Comparable<Estado> {
+
+    private String sigla;
+    private String nome;
+
+    public Estado(String sigla, String nome) {
+        Objects.requireNonNull(sigla);
+        Objects.requireNonNull(nome);
+        this.sigla = sigla;
+        this.nome = nome;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Estado{" +
+                "sigla='" + sigla + '\'' +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Estado o) {
+        return getNome().compareTo(o.getNome());
+    }
+}
