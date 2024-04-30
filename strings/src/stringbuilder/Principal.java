78c9c5bc2814fc1d33e049081eb24788f776530c
Silas Buarque <silasbuarque@gmail.com>
2024-04-30 07:01:55 -0300

    Usando StringBuilder

---
+++ strings/src/stringbuilder/Principal.java
@@ -0,0 +1,25 @@
+package stringbuilder;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        long tempoInicio = System.currentTimeMillis();
+
+//        String texto = "";
+        StringBuilder sb = new StringBuilder(250_000);
+
+        for (int i = 0; i < 200_000; i++) {
+//            texto = texto + "#";
+            sb.append("#");
+        }
+
+//        System.out.println(texto);
+        System.out.println(sb.toString());
+
+        double duracao = (System.currentTimeMillis() - tempoInicio) / 1000d;
+        System.out.printf("Duração: %.2fs%n", duracao);
+
+    }
+
+}
