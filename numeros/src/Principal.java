e3e2656799e40ea35cd0282cec03a4fcf27fe778
Silas Buarque <silasbuarque@gmail.com>
2024-04-30 08:17:04 -0300

    Usando equals() para comparar tipos wrappers

---
+++ numeros/src/Principal.java
@@ -0,0 +1,16 @@
+public class Principal {
+
+    public static void main(String[] args) {
+
+        int x = 127;
+        int y = 127;
+
+        Integer a = 127;
+        Integer b = 127;
+
+        System.out.println(x == y);
+        System.out.println(a.equals(b));
+
+    }
+
+}
