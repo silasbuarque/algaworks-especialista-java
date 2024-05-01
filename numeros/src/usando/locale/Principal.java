4cbb22c73dd6cbf3e0bb9cdbb75e90b6892c9c6d
Silas Buarque <silasbuarque@gmail.com>
2024-05-01 14:39:12 -0300

    Usando Locale para especificar a localizacao

---
+++ numeros/src/usando/locale/Principal.java
@@ -0,0 +1,33 @@
+package usando.locale;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+//        Locale.setDefault(Locale.US);
+//        Locale.setDefault(new Locale("pt","BR"));
+
+        double a = 4_124_298.4238;
+        float b = 0.3f;
+        int c = -1;
+        BigDecimal d = new BigDecimal("45.362");
+
+//        DecimalFormatSymbols symbols = new DecimalFormatSymbols(new Locale("pt", "BR"));
+//        NumberFormat formatador = new DecimalFormat("\u00A4 #,##0.00;(\u00A4 #,##0.00)", symbols);
+
+        NumberFormat formatador = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+
+        System.out.println(formatador.format(a));
+        System.out.println(formatador.format(b));
+        System.out.println(formatador.format(c));
+        System.out.println(formatador.format(d));
+
+    }
+
+}
