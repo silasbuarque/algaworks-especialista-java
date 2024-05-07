21d26a7e6b68bb13f98d4f9ed172eea02969c451
Silas Buarque <silasbuarque@gmail.com>
2024-05-07 06:18:52 -0300

    Transformando String em Calendar

--- Códigos/22-date-e-calendar/src/datas/UsandoDataComoInput.java
+++ Códigos/22-date-e-calendar/src/datas/UsandoDataComoInput.java
@@ -1,2 +1,35 @@
-package datas;public class UsandoDataComoInput {
+package datas;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+
+public class UsandoDataComoInput {
+
+    public static void main(String[] args) throws ParseException {
+
+        DateFormat formatador = new SimpleDateFormat("dd/MM/yyyy");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Data de aniversário: ");
+        String dataAniversarioTexto = scanner.nextLine();
+
+        Date dataNascimento = formatador.parse(dataAniversarioTexto);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dataNascimento);
+
+        System.out.println(calendar.getTime());
+
+        if (calendar.get(Calendar.MONTH) == Calendar.DECEMBER
+                && calendar.get(Calendar.DAY_OF_MONTH) == 25) {
+            System.out.println("Feliz aniversário e feliz natal");
+        } else if (calendar.get(Calendar.DAY_OF_YEAR) == 256) {
+            System.out.println("Você nasceu no dia do programador");
+        }
+    }
+
 }
