a478ff5fcbef6f7292508cc29e1792f4e910a19f
Silas Buarque <silasbuarque@gmail.com>
2024-05-25 19:28:04 -0300

    Aprendendo OffsetDateTime e OffsetTime

---
+++ Códigos/23-date-time-api/src/codigos/OffsetDateTimeEOffsetTime/Principal2.java
@@ -0,0 +1,15 @@
+package codigos.OffsetDateTimeEOffsetTime;
+
+import java.time.OffsetTime;
+
+public class Principal2 {
+
+    public static void main(String[] args) {
+
+        OffsetTime hora = OffsetTime.now();
+
+        System.out.println(hora);
+
+    }
+
+}
