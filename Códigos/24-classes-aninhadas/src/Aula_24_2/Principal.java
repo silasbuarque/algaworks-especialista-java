ff2ad80eb7b743eb6fe3e7d6bb92cf43fe749335
Silas Buarque <silasbuarque@gmail.com>
2024-05-26 08:39:18 -0300

    Classes aninhadas

---
+++ Códigos/24-classes-aninhadas/src/Aula_24_2/Principal.java
@@ -0,0 +1,15 @@
+package Aula_24_2;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        ServicoEmail servicoEmail = new ServicoEmail();
+        ServicoEmail.Mensagem mensagem = new ServicoEmail.Mensagem("joão@algaworks.com", "maria@algaworks.com",
+                "Olá Mundo.");
+
+        servicoEmail.enviar(mensagem);
+
+    }
+
+}
